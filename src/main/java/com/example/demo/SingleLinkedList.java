4fb02e2b63b16c9e75da4fb3ef51c5b0e9330694
Arhun <bmallikarujuna66@gmail.com>
2021-12-20 21:17:28 -0600

    SingleLinkedList

---
+++ src/main/java/com/example/demo/SingleLinkedList.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+public class SingleLinkedList {
+
+	Node head = null;
+
+	class Node {
+		int data;
+		Node next;
+
+		public Node() {
+			super();
+		}
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public void insert(int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		if (head != null) {
+			Node n = head;
+			while (n.next != null) {
+				n = n.next;
+			}
+			n.next = node;
+		} else {
+			head = node;
+		}
+	}
+
+	public void display() {
+		Node n = head;
+		System.out.println(n.data);
+		while (n.next != null) {
+			n = n.next;
+			System.out.println(n.data);
+		}
+	}
+
+	public static void main(String[] args) {
+		SingleLinkedList linkedList = new SingleLinkedList();
+		linkedList.insert(5);
+		linkedList.insert(10);
+
+		linkedList.display();
+	}
+}
